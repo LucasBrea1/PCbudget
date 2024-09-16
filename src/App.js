64c91e79ff53998339aa168c5ec99c17aae6135a
LucasBrea1 <lucasbreaxd@gmail.com>
2024-09-16 16:43:40 -0300

    brea progreso

--- src/App.js
+++ src/App.js
@@ -1,23 +1,21 @@
-import logo from './logo.svg';
 import './App.css';
+import { BrowserRouter as Router, Route, Routes, Link } from 'react-router-dom';
+import Carrito from './pages/carrito';
+import Home from './pages/home';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <Router>
+        <nav>
+          <Link to="/">Home</Link> | <Link to="/carrito">carrito</Link>
+        </nav>
+
+        <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/carrito" element={<Carrito/>} />
+        </Routes>
+      </Router>
     </div>
   );
 }
