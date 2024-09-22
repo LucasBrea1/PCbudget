4a0bc9058c342bfb7a9a758ab3aa3d16543a37d7
LucasBrea1 <lucasbreaxd@gmail.com>
2024-09-22 14:28:56 -0300

    primer carrito

--- src/pages/Carrito.js
+++ src/pages/Carrito.js
@@ -1,11 +1,47 @@
-import React from 'react'
+import React from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { addToCart, removeFromCart, clearCart } from '../redux/CartSlice';
+
 
 function Carrito() {
+  const dispatch = useDispatch();
+  const cartItems = useSelector((state) => state.cart.items);
+  const totalAmount = useSelector((state) => state.cart.totalAmount);
+
+  const handleAddToCart = (item) => {
+    dispatch(addToCart(item));
+  };
+
+  const handleRemoveFromCart = (id) => {
+    dispatch(removeFromCart(id));
+  };
+
+  const handleClearCart = () => {
+    dispatch(clearCart());
+  };
+
   return (
     <div>
-      carrito
+    <h2>Shopping Cart</h2>
+    {cartItems.length === 0 ? (
+      <p>Your cart is empty</p>
+    ) : (
+      <ul>
+        {cartItems.map((item) => (
+          <li key={item.id}>
+            {item.name} - ${item.price} (x{item.quantity})
+            <button onClick={() => handleAddToCart(item)}>Add one more</button>
+            <button onClick={() => handleRemoveFromCart(item.id)}>Remove</button>
+          </li>
+        ))}
+      </ul>
+    )}
+    <div>
+      <p>Total: ${totalAmount}</p>
+      <button onClick={handleClearCart}>Clear Cart</button>
     </div>
-  )
+  </div>
+  );
 }
 
 export default Carrito
